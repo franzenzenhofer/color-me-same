a25961ac9c5af672a07d9812e0d13884172793c7
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2025-07-24 20:43:49 +0000

    test: Add comprehensive unit tests for Tile component (22 tests)

--- src/version.ts
+++ src/version.ts
@@ -1,14 +1,14 @@
 // Auto-generated at build time - DO NOT EDIT
 export const VERSION_INFO = {
-  "version": "1.29.1",
-  "buildDate": "2025-07-24T20:42:48.342Z",
-  "buildTimestamp": 1753389768342,
-  "gitCommit": "49d087e4d78b44c25dee9c63808c0b3fc70ac261",
+  "version": "1.29.2",
+  "buildDate": "2025-07-24T20:43:55.344Z",
+  "buildTimestamp": 1753389835344,
+  "gitCommit": "2756012efd90c56c6a57c36524b74ebfb3b69cdb",
   "gitBranch": "master",
   "environment": "development",
-  "displayVersion": "v1.29.1",
-  "displayDate": "7/24/2025 8:42:48 PM",
-  "displayCommit": "49d087e"
+  "displayVersion": "v1.29.2",
+  "displayDate": "7/24/2025 8:43:55 PM",
+  "displayCommit": "2756012"
 } as const;
 
 export const {
