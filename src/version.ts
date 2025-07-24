f24e218b2cca86bef82e73ca51a8522859a7b8e4
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2025-07-24 20:47:45 +0000

    test: Add comprehensive unit tests for VictoryModal (17 tests)

--- src/version.ts
+++ src/version.ts
@@ -1,14 +1,14 @@
 // Auto-generated at build time - DO NOT EDIT
 export const VERSION_INFO = {
-  "version": "1.31.1",
-  "buildDate": "2025-07-24T20:46:34.059Z",
-  "buildTimestamp": 1753389994059,
-  "gitCommit": "4811c2d60594bf9bd7c6bf956ad370fb1c2bb941",
+  "version": "1.31.2",
+  "buildDate": "2025-07-24T20:47:51.735Z",
+  "buildTimestamp": 1753390071735,
+  "gitCommit": "1b74015c95a2658bde8ec5bcba7b6631dc0be193",
   "gitBranch": "master",
   "environment": "development",
-  "displayVersion": "v1.31.1",
-  "displayDate": "7/24/2025 8:46:34 PM",
-  "displayCommit": "4811c2d"
+  "displayVersion": "v1.31.2",
+  "displayDate": "7/24/2025 8:47:51 PM",
+  "displayCommit": "1b74015"
 } as const;
 
 export const {
