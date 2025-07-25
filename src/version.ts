b04ff8e2aed54a08403f54c8a86f3d3034988da8
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2025-07-25 09:50:37 +0000

    chore: release v1.46.0

--- src/version.ts
+++ src/version.ts
@@ -1,14 +1,14 @@
 // Auto-generated at build time - DO NOT EDIT
 export const VERSION_INFO = {
-  "version": "1.45.2",
-  "buildDate": "2025-07-25T09:49:55.446Z",
-  "buildTimestamp": 1753436995446,
-  "gitCommit": "d35d6c7fd327637f71dcb9fb35c604e19708128b",
+  "version": "1.46.1",
+  "buildDate": "2025-07-25T09:50:42.736Z",
+  "buildTimestamp": 1753437042737,
+  "gitCommit": "cca20a69a96de2469a8b2d4fce2b91297a521ed3",
   "gitBranch": "master",
   "environment": "development",
-  "displayVersion": "v1.45.2",
-  "displayDate": "7/25/2025 9:49:55 AM",
-  "displayCommit": "d35d6c7"
+  "displayVersion": "v1.46.1",
+  "displayDate": "7/25/2025 9:50:42 AM",
+  "displayCommit": "cca20a6"
 } as const;
 
 export const {
